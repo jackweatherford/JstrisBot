da53dd6acbb233766cadf77878457037d9d91a60
jackweatherford <jack.weatherford@gmail.com>
2020-08-29 23:41:59 -0700

    placeGreenZ() working

--- jstrisBot.py
+++ jstrisBot.py
@@ -553,77 +553,141 @@ def placeT():
 
 	pyautogui.press('c')
 
-def placeGreenZ():
-
+def bestGreenZ(moves):
+	best_diff_sum = 200
+	best_move = -1
+	rot = -1
+	
 	# 0 rotations
-	if top[0] == top[1] == top[2] - 1 and abs(top[2] + 1 - top[3]) < 3:
-		top[0] += 1
-		top[1] += 2
-		top[2] += 1
+	for move in moves[0]:
+		temp_top = top.copy()
+		temp_top[move] += 1
+		temp_top[move+1] += 2
+		temp_top[move+2] += 1
 		
-		pyautogui.press('left', presses=3)
-		pyautogui.press('space')
-		return
+		diff_sum = 0
+		for i in range(9):
+			diff_sum += abs(temp_top[i+1] - temp_top[i])
+		
+		if abs(diff_sum) < best_diff_sum:
+			best_diff_sum = abs(diff_sum)
+			best_move = move
+			rot = 0
+	
+	# 1 rotation
+	for move in moves[1]:
+		temp_top = top.copy()
+		temp_top[move] += 2
+		temp_top[move+1] += 2
+		
+		diff_sum = 0
+		for i in range(9):
+			diff_sum += abs(temp_top[i+1] - temp_top[i])
+		
+		if abs(diff_sum) < best_diff_sum:
+			best_diff_sum = abs(diff_sum)
+			best_move = move
+			rot = 1
+	
+	return best_move, rot
+
+# Move format => moves<# rotations> = {column index : (left diff, right diff), ... }
+def placeGreenZ():
+	# 0 rotations
+	moves0 = {}
+	if top[0] == top[1] == top[2] - 1:
+		moves0[0] = (0, diff[2] - 1)
 	
 	for i in range(1, 7):
-		if top[i] == top[i+1] == top[i+2] - 1 and abs(top[i] + 1 - top[i-1]) < 3 and abs(top[i+2] + 1 - top[i+3]) < 3:
-			if i < 3:
-				pyautogui.press('left', presses=3-i)
-			elif i > 3:
-				pyautogui.press('right', presses=i-3)
-
-			top[i] += 1
-			top[i+1] += 2
-			top[i+2] += 1
-
-			pyautogui.press('space')
-			return
+		if top[i] == top[i+1] == top[i+2] - 1:
+			moves0[i] = (diff[i-1] + 1, diff[i+2] - 1)
 	
-	if top[7] == top[8] == top[9] - 1 and abs(top[7] + 1 - top[6]) < 3:
-		top[7] += 1
-		top[8] += 2
-		top[9] += 1
-		
-		pyautogui.press('right', presses=4)
-		pyautogui.press('space')
-		return
-
+	if top[7] == top[8] == top[9] - 1:
+		moves0[7] = (diff[6] + 1, 0)
+	
 	# 1 rotation
-	if top[0] - 1 == top[1] and abs(top[1] + 2 - top[2]) < 3:
-		top[0] += 2
-		top[1] += 2
-		
-		pyautogui.press('up')
-		pyautogui.press('left', presses=4)
-		pyautogui.press('space')
-		return
+	moves1 = {}
+	if top[0] - 1 == top[1]:
+		moves1[0] = (0, diff[1] - 2)
 	
 	for i in range(1, 8):
-		if top[i] - 1 == top[i+1] and abs(top[i] + 2 - top[i-1]) < 3 and abs(top[i+1] + 2 - top[i+2]) < 3:
-			
-			top[i] += 2
-			top[i+1] += 2
-			
-			pyautogui.press('up')
-			
-			if i < 4:
-				pyautogui.press('left', presses=4-i)
-			elif i > 4:
-				pyautogui.press('right', presses=i-4)
-
-			pyautogui.press('space')
-			return
+		if top[i] - 1 == top[i+1]:
+			moves1[i] = (diff[i-1] + 2, diff[i+1] - 2)
 	
-	if top[8] - 1 == top[9] and abs(top[8] + 2 - top[7]) < 3:
-		top[8] += 2
-		top[9] += 2
+	if top[8] - 1 == top[9]:
+		moves1[8] = (diff[7] + 2, 0)
+	
+	# moves with no holes
+	moves_no_holes = [moves0, moves1]
+	
+	moves_no_3 = filterMoves(moves_no_holes)
+	
+	if moves_no_3 == -1: # c pressed
+		return
+	
+	# 0 rotations
+	moves0 = []
+	for i in moves_no_3[0]: # get moves that can reduce >=3 walls
+		if i == 0:
+			if diff[2] >= 3:
+				moves0.append(i)
+			break
+		if i == 7:
+			if diff[6] <= -3:
+				moves0.append(i)
+			break
+		if diff[i + 2] >= 3 or diff[i - 1] <= -3:
+			moves0.append(i)
+	
+	# 1 rotation
+	moves1 = []
+	for i in moves_no_3[1]:
+		if i == 0:
+			if diff[1] >= 3:
+				moves1.append(i)
+			break
+		if i == 8:
+			if diff[7] <= -3:
+				moves1.append(i)
+			break
+		if diff[i + 1] >= 3 or diff[i - 1] <= -3:
+			moves1.append(i)
+	
+	moves_reducing = [moves0, moves1]
+	
+	if len(moves_no_3[0]) == 0 and len(moves_no_3[1]) == 0: # no moves create < 3 walls
+		if len(moves0) == 0 and len(moves1) == 0: # no walls can be reduced, choose the move that results in sum of diffs being closest to 0
+			best_move, rot = bestGreenZ(moves_no_holes)
+		else: # choose wall reduction that reults in sum of diffs being closest to 0
+			best_move, rot = bestGreenZ(moves_reducing)
+	else: # at least 1 move in moves_no_3
+		if len(moves0) == 0 and len(moves1) == 0: # no walls can be reduced, choose the move that results in sum of diffs being closest to 0
+			best_move, rot = bestGreenZ(moves_no_3)
+		else: # at least 1 move can reduce walls
+			best_move, rot = bestGreenZ(moves_reducing)
+	
+	# 0 rotations
+	if rot == 0:
+		top[best_move] += 1
+		top[best_move+1] += 2
+		top[best_move+2] += 1
+		
+		if best_move < 3:
+			pyautogui.press('left', presses=3-best_move)
+		elif best_move > 3:
+			pyautogui.press('right', presses=best_move-3)
+		pyautogui.press('space')
+	
+	else: # 1 rotation
+		top[best_move] += 2
+		top[best_move+1] += 2
 		
 		pyautogui.press('up')
-		pyautogui.press('right', presses=4)
+		if best_move < 4:
+			pyautogui.press('left', presses=4-best_move)
+		elif best_move > 4:
+			pyautogui.press('right', presses=best_move-4)
 		pyautogui.press('space')
-		return
-
-	pyautogui.press('c')
 
 def placeRedZ():
 
